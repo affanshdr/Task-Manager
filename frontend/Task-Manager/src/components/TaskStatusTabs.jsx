2e04b7639fa40f5472fead1513074d9b7c1a8d75
Affan Suhendar <affansuhendar1@gmail.com>
2025-05-23 11:50:30 +0700

    Update

--- frontend/Task-Manager/src/components/TaskStatusTabs.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const TaskStatusTabs = () => {
-    return (
-        <div className="">TaskStatusTabs</div>
-    )
-}
-
-export default TaskStatusTabs;
--- frontend/Task-Manager/src/pages/Admin/ManageUsers.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ManageUsers = () => {
-  return <div className="text-6xl font-bold">ManageUsers</div>;
-};
-
-export default ManageUsers;
